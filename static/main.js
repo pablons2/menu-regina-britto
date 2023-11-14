87f2ee1288986f201882dadf33bec8eda7a0fbf0
pablons2 <pablonicholas2012@gmail.com>
2023-11-14 00:22:28 -0300

    finish | project

---
+++ static/main.js
@@ -0,0 +1,15 @@
+
+
+const modal_clinicas= document.getElementById("modal-clinicas")
+
+
+
+const OpenModal = ()=>{
+
+   modal_clinicas.classList.add('flex')
+
+}
+
+const FecharModal = () =>{
+    modal_clinicas.classList.remove('flex')
+}
