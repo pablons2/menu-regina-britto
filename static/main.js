0f1fc89bce5e3d73ff925308df2e492204140219
pablons2 <pablonicholas2012@gmail.com>
2024-08-10 11:07:42 -0300

    feat(tela-curriculo): criei a rota para vizualiação do curriculo

--- static/main.js
+++ static/main.js
@@ -17,17 +17,17 @@ const id_list_menu_principal = document.getElementById("list-menu-principal");
 
 const list_menu_principal = [
     {
-        "class": "text-center p-2 bg-white border-gray-300 border-2 hover:bg-pink-600 hover:text-white text-pink-400  h-[4rem] border rounded-[20rem] cursor-pointer w-[90%]",
+        "class": "text-center p-2 bg-white border-gray-300 border-2 hover:bg-pink-600 hover:text-white text-pink-400  h- border rounded-[20rem] cursor-pointer w-[90%]",
         "item_name": "Maceió",
         "link": "https://wa.me/5582988287425?text=Ol%C3%A1+Nutri+Regina+Britto%2C+vim+pelo+seu+link+e+quero+agendar+uma+consulta%21",
     },
     {
-        "class": "text-center p-2 bg-white border-gray-300 border-2 hover:bg-pink-600 hover:text-white text-pink-400  h-[4rem] border rounded-[20rem] cursor-pointer w-[90%]",
+        "class": "text-center p-2 bg-white border-gray-300 border-2 hover:bg-pink-600 hover:text-white text-pink-400  h- border rounded-[20rem] cursor-pointer w-[90%]",
         "item_name": "Piranhas",
         "link": "https://wa.me/5582988287425?text=Ol%C3%A1+Nutri+Regina+Britto%2C+vim+pelo+seu+link+e+quero+agendar+uma+consulta%21",
     },
     {
-        "class": "text-center p-2 bg-white border-gray-300 border-2 hover:bg-pink-600 hover:text-white text-pink-400  h-[4rem] border rounded-[20rem] cursor-pointer w-[90%]",
+        "class": "text-center p-2 bg-white border-gray-300 border-2 hover:bg-pink-600 hover:text-white text-pink-400  h- border rounded-[20rem] cursor-pointer w-[90%]",
         "item_name": "Clínicas",
         "onclick": "OpenModal()"
     },
