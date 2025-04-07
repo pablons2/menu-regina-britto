3cc887d42b24a0498564354241cb582e8eedde4d
pabloN <pablonicholas2012@gmail.com>
2025-04-06 22:12:58 -0300

    feat( nova identidade visual )

--- static/main.js
+++ static/main.js
@@ -1,59 +1,56 @@
+const modal_clinicas = document.getElementById('modal-clinicas')
 
-const modal_clinicas= document.getElementById("modal-clinicas")
-
-const OpenModal = ()=>{
-
-   modal_clinicas.classList.add('flex')
-
+const OpenModal = () => {
+  modal_clinicas.classList.add('flex')
 }
 
-const FecharModal = () =>{
-    modal_clinicas.classList.remove('flex')
+const FecharModal = () => {
+  modal_clinicas.classList.remove('flex')
 }
 
-
-
-const id_list_menu_principal = document.getElementById("list-menu-principal");
+const id_list_menu_principal = document.getElementById('list-menu-principal')
 
 const list_menu_principal = [
-    {
-        "class": " text-center p-2 bg-white border-gray-300 border-2 hover:bg-pink-600 hover:text-white text-pink-400  h- border rounded-[20rem] cursor-pointer w-[90%]",
-        "item_name": "F. de Pré Cadastro",
-        "link": "https://form.jotform.com/243107544269660",
-    },
-    {
-        "class": " text-center p-2 bg-white border-gray-300 border-2 hover:bg-pink-600 hover:text-white text-pink-400  h- border rounded-[20rem] cursor-pointer w-[90%]",
-        "item_name": "Maceió",
-        "link": "https://wa.me/5582988287425?text=Ol%C3%A1+Nutri+Regina+Britto%2C+vim+pelo+seu+link+e+quero+agendar+uma+consulta%21",
-    },
-    {
-        "class": " text-center p-2 bg-white border-gray-300 border-2 hover:bg-pink-600 hover:text-white text-pink-400  h- border rounded-[20rem] cursor-pointer w-[90%]",
-        "item_name": "Piranhas",
-        "link": "https://wa.me/5582988287425?text=Ol%C3%A1+Nutri+Regina+Britto%2C+vim+pelo+seu+link+e+quero+agendar+uma+consulta%21",
-    },
-    {
-        "class": " text-center p-2 bg-white border-gray-300 border-2 hover:bg-pink-600 hover:text-white text-pink-400  h- border rounded-[20rem] cursor-pointer w-[90%]",
-        "item_name": "Clínicas",
-        "onclick": "OpenModal()"
-    },
-
-   
-];
+  {
+    class:
+      ' bg-[#f8d9de] text-[#5e2c35] font-medium py-4 rounded-full shadow-sm transition hover:brightness-95 w-[90%]  cursor-pointer',
+    item_name: 'F. de Pré Cadastro',
+    link: 'https://form.jotform.com/243107544269660',
+  },
+  {
+    class:
+      'bg-[#c9d7b5] text-[#374030] font-medium py-4 rounded-full shadow-sm transition hover:brightness-95 w-[90%]  cursor-pointer',
+    item_name: 'Maceió',
+    link: 'https://wa.me/5582988287425?text=Ol%C3%A1+Nutri+Regina+Britto%2C+vim+pelo+seu+link+e+quero+agendar+uma+consulta%21',
+  },
+  {
+    class:
+      'bg-[#c9d7b5] text-[#374030] font-medium py-4 rounded-full shadow-sm transition hover:brightness-95 w-[90%]  cursor-pointer',
+    item_name: 'Piranhas',
+    link: 'https://wa.me/5582988287425?text=Ol%C3%A1+Nutri+Regina+Britto%2C+vim+pelo+seu+link+e+quero+agendar+uma+consulta%21',
+  },
+  {
+    class:
+      'bg-[#c9d7b5] text-[#374030] font-medium py-4 rounded-full shadow-sm transition hover:brightness-95 w-[90%]  cursor-pointer',
+    item_name: 'Clínicas',
+    onclick: 'OpenModal()',
+  },
+]
 
 list_menu_principal.map((item) => {
-    if (item.item_name === "Clínicas") {
-        id_list_menu_principal.innerHTML += `<li class="${item.class}" onclick="${item.onclick}"><a href="#">${item.item_name}</a></li>`;
-    } else {
-        id_list_menu_principal.innerHTML += `<li class="${item.class}"><a href="${item.link}">${item.item_name}</a></li>`;
-    }
-});
+  if (item.item_name === 'Clínicas') {
+    id_list_menu_principal.innerHTML += `<li class="${item.class}" onclick="${item.onclick}"><a href="#">${item.item_name}</a></li>`
+  } else {
+    id_list_menu_principal.innerHTML += `<li class="${item.class}"><a href="${item.link}">${item.item_name}</a></li>`
+  }
+})
 
 // Open and close sidebar
 function openNav() {
-    document.getElementById("mySidebar").style.width = "60%";
-    document.getElementById("mySidebar").style.display = "block";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.display = "none";
-  }
+  document.getElementById('mySidebar').style.width = '60%'
+  document.getElementById('mySidebar').style.display = 'block'
+}
+
+function closeNav() {
+  document.getElementById('mySidebar').style.display = 'none'
+}
